f564a8d86c771bd22edadf303034b0df5d2a020e
introvertCode <stanislaw.bielski.programista@gmail.com>
2021-02-10 20:39:30 +0100

    massive update and use of bootstrap

---
+++ js/animation.js
@@ -0,0 +1,19 @@
+
+// $(document).ready(function(){
+$("#show-bal").css("top", "220px");
+	 
+	 $("#period").change(function(){
+		let val = $("#period option:selected").text();
+		// alert(val);
+		if( val === "niestandardowy") {
+			$("#form_container").height(360);
+			$("#select-date").fadeIn(300);
+			$("#show-bal").css("top", "420px");
+		}
+		else {
+			$("#form_container").height(160);
+			$("#select-date").fadeOut(300);
+			$("#show-bal").css("top", "220px");
+			
+		}
+	  });
